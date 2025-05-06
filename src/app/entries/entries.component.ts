d47b2ac6f3526c0f09507fdaa92b8cea82e129e2
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-05-06 16:42:38 +0200

    added search-functionality

--- src/app/entries/entries.component.ts
+++ src/app/entries/entries.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { EntryComponent } from './entry/entry.component';
 import { ApiService } from '../api.service';
 import { PhonebookEntry } from '../models/phonebook-entry';
@@ -7,22 +8,34 @@ import { PhonebookEntry } from '../models/phonebook-entry';
 @Component({
   selector: 'app-entries',
   standalone: true,
-  imports: [EntryComponent, CommonModule],
+  imports: [EntryComponent, CommonModule, FormsModule],
   templateUrl: './entries.component.html',
   styleUrl: './entries.component.scss',
 })
 export class EntriesComponent implements OnInit {
   entries: PhonebookEntry[] = [];
+  searchTerm: string = '';
+  private currentApiUrl: string = '';
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
     this.apiService.getApiUrl().subscribe((apiUrl) => {
       if (apiUrl) {
-        this.apiService
-          .getEntries(apiUrl)
-          .subscribe((entries) => (this.entries = entries));
+        this.currentApiUrl = apiUrl;
+        this.loadEntries();
       }
     });
+  }
+
+  onSearch(event: any) {
+    this.searchTerm = event.target.value;
+    this.loadEntries();
+  }
+
+  private loadEntries() {
+    this.apiService
+      .getEntries(this.currentApiUrl, this.searchTerm)
+      .subscribe((entries) => (this.entries = entries));
   }
 }
