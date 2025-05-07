ce5b7e9cf42eeec5fa20690008aebae1d97571a4
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-05-07 09:15:56 +0200

    added functionality to create new entry

--- src/app/entries/entries.component.ts
+++ src/app/entries/entries.component.ts
@@ -70,4 +70,29 @@ export class EntriesComponent implements OnInit, OnDestroy {
   showEntryDetail() {
     this.entryDetail.show();
   }
+
+  addNewEntry(entryData: Omit<PhonebookEntry, 'id'>) {
+    const newId = this.getNextAvailableId();
+    const newEntry: PhonebookEntry = {
+      id: newId,
+      ...entryData,
+    };
+
+    this.entries = [...this.entries, newEntry];
+  }
+
+  private getNextAvailableId(): number {
+    if (this.entries.length === 0) {
+      return 1;
+    }
+
+    const usedIds = new Set(this.entries.map((entry) => entry.id));
+    let nextId = 1;
+
+    while (usedIds.has(nextId)) {
+      nextId++;
+    }
+
+    return nextId;
+  }
 }
--- src/app/entries/entry-detail/entry-detail.component.ts
+++ src/app/entries/entry-detail/entry-detail.component.ts
@@ -1,10 +1,12 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PhonebookEntry } from '../../models/phonebook-entry';
 
 @Component({
   selector: 'app-entry-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './entry-detail.component.html',
   styleUrl: './entry-detail.component.scss',
 })
@@ -12,11 +14,36 @@ export class EntryDetailComponent {
   @HostBinding('class.visible')
   isVisible = false;
 
+  @Output() save = new EventEmitter<Omit<PhonebookEntry, 'id'>>();
+
+  entry: Omit<PhonebookEntry, 'id'> = {
+    vorname: '',
+    nachname: '',
+    telefonVorwahl: '',
+    telefonnummer: '',
+  };
+
   show() {
     this.isVisible = true;
+    this.resetForm();
   }
 
   hide() {
     this.isVisible = false;
+    this.resetForm();
+  }
+
+  onSave() {
+    this.save.emit(this.entry);
+    this.hide();
+  }
+
+  private resetForm() {
+    this.entry = {
+      vorname: '',
+      nachname: '',
+      telefonVorwahl: '',
+      telefonnummer: '',
+    };
   }
 }
