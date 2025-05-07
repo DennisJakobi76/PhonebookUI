a73da261ded8e818693d173901d4b8aaacbe54bc
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-05-07 09:03:43 +0200

    added click-events to buttons to open and close entry-detail-component

--- src/app/entries/entry-detail/entry-detail.component.ts
+++ src/app/entries/entry-detail/entry-detail.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-entry-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './entry-detail.component.html',
-  styleUrl: './entry-detail.component.scss'
+  styleUrl: './entry-detail.component.scss',
 })
 export class EntryDetailComponent {
+  @HostBinding('class.visible')
+  isVisible = false;
 
+  show() {
+    this.isVisible = true;
+  }
+
+  hide() {
+    this.isVisible = false;
+  }
 }
